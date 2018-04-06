7b20fb27160748ee7090eacffc036d0397a43b8c
Chris Owen <chris@thecodethatchriswrote.com>
2018-04-06 03:24:40 +0300

    More refactoring to make the code smaller

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /*jshint esversion: 6 */
 
+//Required Libs
 const express = require('express');
 const fetch = require("node-fetch");
 const TranslatorBot = require('./translator_bot.js');
@@ -8,29 +9,28 @@ const MongoClient = require('mongodb').MongoClient;
 const clientId = process.env.CLIENT_ID.trim();
 const clientSecret = process.env.CLIENT_SECRET.trim();
 
-// var localOAuth = process.env.APP_TOKEN.trim();
-// var localBotOAuth = process.env.TOKEN.trim();
-
 const dbUser = process.env.DB_USER.trim();
 const dbPass = process.env.DB_PASS.trim();
 const uri = `mongodb+srv://${dbUser}:${dbPass}@translatorbot-izwur.mongodb.net/test`;
 
-const app = express();
-const port = process.env.PORT || 8080;
+// var localOAuth = process.env.APP_TOKEN.trim();
+// var localBotOAuth = process.env.TOKEN.trim();
 
 // if (localOAuth != null && localBotOAuth != null) {
 //     new TranslatorBot(localOAuth, localBotOAuth);
 //     saveToMongo(localOAuth, localBotOAuth, "123");
 // }
 
+
 readAllFromMongo();
 
+const app = express();
 app.get('/oauth', function (req, res) {
     res.send('You have now added TranslatorBot to your workspace.\n\nIn Slack, you can invite "TranslatorBot" to any channel to have him start translating messages.');
     let code = req.param('code');
     getOauthToken(clientId, clientSecret, code);
 });
-app.listen(port);
+app.listen(process.env.PORT || 8080);
 
 function getOauthToken(clientId, clientSecret, code) {
     let data = {
@@ -39,7 +39,7 @@ function getOauthToken(clientId, clientSecret, code) {
         "code": code
     };
 
-    fetch('https://slack.com/api/oauth.access?' + encodeQueryData(data), {
+    fetch(`https://slack.com/api/oauth.access?${encodeQueryData(data)}`, {
             method: 'POST',
             mode: 'cors',
             redirect: 'follow',
@@ -62,15 +62,9 @@ function readAllFromMongo() {
         const collection = client.db("users").collection("keys");
 
         var stream = collection.find().stream();
-        stream.on('data', doc => {
-            new TranslatorBot(doc.accessToken, doc.accessBotToken, doc.botId);
-        });
-        stream.on('error', err => {
-            console.log(err);
-        });
-        stream.on('end', () => {
-            client.close();
-        });
+        stream.on('data', doc => new TranslatorBot(doc.accessToken, doc.accessBotToken, doc.botId));
+        stream.on('error', err => console.log(err));
+        stream.on('end', () => client.close());
     });
 }
 
@@ -87,9 +81,7 @@ function saveToMongo(accessToken, accessBotToken, botId) {
 
         collection.save(obj, {
             w: 1
-        }, (err, result) => {
-            client.close();
-        });
+        }, (err, result) => client.close());
     });
 }
 
